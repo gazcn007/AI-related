a42edacf5549a5c0dbe3f2c3e7c23f168608e788
Carl Liu <shikang.liu@mail.mcgill.ca>
2016-01-29 01:05:23 -0500

    a module to convert unicode to strings

---
+++ byteify.py
@@ -0,0 +1,12 @@
+#a short and simple recursive function will convert any decoded 
+#JSON object from using unicode strings in the assignment file to UTF-8-encoded byte strings
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key): byteify(value)
+                for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
